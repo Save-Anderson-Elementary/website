09da7d1af34653b5710f13edec3c3d672ffff641
Coderrob <Rob@Coderrob.com>
2022-09-25 17:12:59 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const uswds = require('@uswds/compile');
+
+/**
+ * USWDS version
+ * Set the version of USWDS you're using (2 or 3)
+ */
+
+uswds.settings.version = 3;
+
+/**
+ * Path settings
+ * Set as many as you need
+ */
+
+uswds.paths.dist.css = './assets/css';
+uswds.paths.dist.sass = './sass';
+
+/**
+ * Exports
+ * Add as many as you need
+ */
+
+exports.init = uswds.init;
+exports.compile = uswds.compile;
